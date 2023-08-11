96a2904c7cc5a70ce6252bc72cc12e95cc07417d
mahlet <akotet2123@gmail.com>
2023-08-11 01:40:28 +0000

    see these

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,26 +1,24 @@
 -- creates a stored procedure AddBonus that adds a new correction for a student.
-DELMITER $$
 DROP PROCEDURE IF EXISTS AddBonus;
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+DELIMITER $$
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-    DECLARE project_id INT;
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
 
-    -- Check if the project exists
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-
-    -- If project doesn't exist, create it
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+        INSERT INTO projects(name)
+            VALUES(project_name);
     END IF;
-
-    -- Insert the new correction
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-
-    -- Update the user's average_score
-    UPDATE users
-    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
-    WHERE id = user_id;
-END;
-$$
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES (user_id, project_id, score);
+END $$
 DELIMITER ;
