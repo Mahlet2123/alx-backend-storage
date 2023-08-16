db8f13096842e09b79d5aa5423e2b85788e80477
mahlet <akotet2123@gmail.com>
2023-08-16 15:30:59 +0000

    return formats of redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,3 +15,22 @@ class Cache():
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        value = self._redis.get(key)
+        if value:
+            if fn:
+                value = fn(value)
+        else:
+            return None
+        return value
+
+    def get_str(self, key: str):
+        return self.get(key, decode_key())
+
+    def get_int(self):
+        return self.get(key, int())
+
+    @staticmethod
+    def decode_key(value: bytes) -> str:
+        return value.decode("utf8")
