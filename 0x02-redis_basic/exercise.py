d4e9077cbf9d381f571f49c38af5a8deab64c67c
mahlet <akotet2123@gmail.com>
2023-08-16 15:35:33 +0000

    added documentation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,15 +8,20 @@ from typing import Union
 class Cache():
     """ Cache class """
     def __init__(self):
+        """ constructor method """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ generate a random key (e.g. using uuid),
+        store the input data in Redis using the random key
+        and return the key. """
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
 
     def get(self, key: str, fn: Optional[Callable] = None):
+        """ get value from redis """
         value = self._redis.get(key)
         if value:
             if fn:
@@ -26,11 +31,14 @@ class Cache():
         return value
 
     def get_str(self, key: str):
+        """ convert redis return value to string """
         return self.get(key, decode_key())
 
     def get_int(self):
+        """ convert redis return value into int """
         return self.get(key, int())
 
     @staticmethod
     def decode_key(value: bytes) -> str:
+        """ decode_key static method """
         return value.decode("utf8")
